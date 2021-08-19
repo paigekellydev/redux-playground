e55e88ef2cd42c18a7272885d9ec7c1e331e3bdc
Paige <72361304+pckellyco@users.noreply.github.com>
2021-08-19 12:06:55 -0600

    Created INCREMENT action

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import { createStore } from 'redux'
 function App() {
 
   let incrementAction = {type: "INCREMENT"}
+  const store = createStore(increment)
   
   const increment = (state=0, action) => {
     switch(action.type) {
       case "INCREMENT":
         return state + 1
-      default:
-        return state
+        default:
+          return state
+        }
       }
-    }
-    
-  const store = createStore(increment)
-  console.log(increment)
+      
+  console.log(store)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        {/* <Counter /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
